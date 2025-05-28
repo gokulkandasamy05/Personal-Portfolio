3b51a308f222da6fd0130feb02d741e88bc6d05d
gokul <gokul@constient.com>
2025-05-28 21:07:01 +0530

    changes

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -9,6 +9,7 @@ import { FaReact, FaHtml5, FaCss3Alt, FaExternalLinkAlt } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io";
 import TransitionOptions from "./TransitionAnimation";
 import { EducationArray, ExperienceArray, PrjectsArray } from "./utils/common";
+import Image from "next/image";
 
 interface refArrayTypes {
     ref_name: React.RefObject<HTMLElement | null>;
@@ -108,19 +109,16 @@ const Main = () => {
                     <div className="md:block hidden md:mt-16">
                         <nav className="flex md:flex-col gap-3">
                             {refArray.map((item, index) => (
-                                <div key={item?.name} onClick={() => scrollToSection(item?.ref_name)} className={`flex items-center gap-4 group cursor-pointer ${item?.is_active ? 'text-white' : 'text-[#94a3b8]'}`}>
-                                    <div className={`h-[2px] w-7 m-0 transition-all duration-300 origin-left ${item?.is_active ? 'bg-white w-32' : 'bg-[#94a3b8] group-hover:w-32 group-hover:bg-white'}`} />
+                                <div key={item?.name} onClick={() => scrollToSection(item?.ref_name)} className={`flex items-center gap-4 group cursor-pointer ${item?.is_active ? 'text-[#5eead4]' : 'text-[#94a3b8]'}`}>
+                                    <div className={`h-[2px] w-7 m-0 transition-all duration-300 origin-left ${item?.is_active ? 'bg-[#5eead4] w-28' : 'bg-[#94a3b8] group-hover:w-28 group-hover:bg-white'}`} />
                                     <EnterAnimation duration={0.4 * (index + 1)}>
-                                        <Link href="" className="transition-colors duration-300 group-hover:text-white font-semibold !text-lg">{item?.name}</Link>
+                                        <Link href="" className={`transition-colors duration-300 ${item?.is_active ? 'group-hover:text-[#5eead4]' : 'group-hover:text-white'}  font-semibold !text-lg`}>{item?.name}</Link>
                                     </EnterAnimation>
 
                                 </div>
                             ))}
                         </nav>
                     </div>
-
-
-
 
                 </div>
             </TextAnimation>
@@ -131,23 +129,23 @@ const Main = () => {
                         <div className="w-full py-5">
                             <h1 className="font-bold text-2xl text-[#fff]">ABOUT</h1>
                         </div>
-                        <div>
+                        <div className="text-lg">
                             I’m Gokul Kandasamy, a frontend developer with 3 years of hands-on experience crafting intuitive and responsive web applications using modern technologies like <span className="text-white font-semibold">React.js</span> and <span className="text-white font-semibold">JavaScript (ES6+)</span>. I thrive on building elegant user interfaces that are fast, accessible, and maintainable.
                         </div>
 
-                        <div className="mt-5">
+                        <div className="mt-5 text-lg">
                             At <span className="text-white font-semibold">Constient Global Solutions</span>, I have led frontend development for enterprise-grade applications, including customer-facing platforms and back-office tools used by over 5,000 Axis Bank employees. My work has directly contributed to increasing engagement and reducing system load times by up to 25%.
                         </div>
 
-                        <div className="mt-5">
+                        <div className="mt-5 text-lg">
                             I’ve built large-scale React applications from the ground up, implemented scalable state management with Redux, and ensured performance optimization across devices. I'm also deeply experienced in API integration, debugging critical production issues, and applying best practices in security and clean code architecture.
                         </div>
 
-                        <div className="mt-5">
+                        <div className="mt-5 text-lg">
                             My tech toolkit includes <span className="text-white font-semibold">Next JS, TypeScript, Tailwind CSS, Material-UI, Git, Webpack</span>, and more. I'm passionate about creating impactful user experiences by blending thoughtful design with solid engineering principles.
                         </div>
 
-                        <div className="mt-5">
+                        <div className="mt-5 text-lg">
                             Currently, I'm open to exciting opportunities where I can contribute to innovative projects, learn from experienced teams, and continue building high-performance, user-focused web applications.
                         </div>
                     </section>
@@ -156,21 +154,27 @@ const Main = () => {
 
 
 
-                <section ref={experienceRef} id='experience' className="md:min-h-screen w-full">
+                <section ref={experienceRef} id='experience' className="md:min-h-screen w-full mt-8">
                     <div className="w-full py-5">
                         <h1 className="font-bold text-2xl text-[#fff]">EXPERIENCE</h1>
                     </div>
                     {
                         ExperienceArray.map(item => {
-                            return <div className="flex flex-col md:flex-row gap-5 w-full border-l border-slate-700 p-8" key={item?.company}>
-                                <div className="md:w-1/4 w-full">
-                                    <p>{item?.duration}</p>
-                                </div>
-                                <div className="md:w-3/4 w-full">
+                            return <div className="flex flex-col md:flex-row gap-8 w-full border-l border-slate-700 md:p-8 p-0 pt-8 pl-8" key={item?.company}>
+                                <div className=" w-full">
                                     <div className="w-full">
-                                        <h1 className="text-[#fff] text-xl">{item?.company}</h1>
-                                        <p className="mt-2 text-[#fdfeff]">{item?.position}</p>
-                                        <div className="mt-3 w-full text-justify">
+                                        <div className="flex flex-row items-center gap-5">
+                                            {item?.logo && <Image src={item?.logo} alt={item?.company} className="h-12 w-12 object-cover rounded-full" />}
+                                            <div>
+                                                <Link href={item?.website} target="_blank">
+                                                    <h1 className="text-[#fff] text-xl font-semibold hover:text-[#5eead4]">{item?.company}</h1>
+                                                </Link>
+                                                <p className="text-[#fdfeff]">{item?.position}</p>
+                                                <p>{item?.duration}</p>
+                                            </div>
+                                        </div>
+
+                                        <div className="mt-4 text-lg w-full text-justify">
                                             <p>{item?.description}</p>
                                         </div>
                                     </div>
@@ -189,22 +193,26 @@ const Main = () => {
 
 
 
-                <section ref={projectsRef} id='projects' className="md:min-h-screen w-full">
+                <section ref={projectsRef} id='projects' className="md:min-h-screen w-full mt-8 md:mt-0">
                     <div className="w-full py-5">
                         <h1 className="font-bold text-2xl text-[#fff]">PROJECTS</h1>
                     </div>
                     {
                         PrjectsArray.map(item => {
-                            return <div className="text-[#fff] w-full border-l border-slate-700 p-8" key={item?.company}>
-                                <h1 className="text-[#fff] text-xl font-semibold">{item?.project_title}</h1>
-                                <p className="text-sm text-[#94a3b8]">{item?.company}</p>
-
+                            return <div className="text-[#fff] w-full border-l border-slate-700 md:p-8 p-0 pt-8 pl-8" key={item?.company}>
+                                <div className="flex flex-row items-center gap-5">
+                                    {item?.logo && <Image src={item?.logo} alt={item?.company} className="h-12 w-12 object-cover rounded-full" />}
+                                    <div>
+                                        <h1 className="text-[#fff] text-xl font-semibold">{item?.project_title}</h1>
+                                        <p className="text-md text-[#94a3b8]">{item?.company}</p>
+                                    </div>
+                                </div>
                                 <div className="mt-5 md:ml-10">
                                     {
                                         item?.projects.map((project, index) => {
                                             return <div key={index} className="border border-slate-700 p-5 mb-3 rounded-lg">
                                                 <h2 className="text-[#fdfeff] text-lg font-semibold">{project?.title}</h2>
-                                                <p className="mt-2 text-[#94a3b8]">{project?.description}</p>
+                                                <p className="mt-2 text-lg text-[#94a3b8] text-justify">{project?.description}</p>
                                             </div>
                                         })
                                     }
@@ -215,27 +223,30 @@ const Main = () => {
                 </section>
 
 
-                <section ref={educationRef} id='education' className="w-full mt-5">
+                <section ref={educationRef} id='education' className="w-full mt-8 md:mt-0">
                     <div className="w-full py-5">
                         <h1 className="font-bold text-2xl text-[#fff]">EDUCATION</h1>
                     </div>
                     {
                         EducationArray.map(item => {
-                            return <div className="flex flex-col md:flex-row gap-5 w-full border-l border-slate-700 p-8" key={item?.institution}>
-                                <div className="md:w-1/4 w-full">
+                            return <div className="flex justify-start flex-col md:flex-row gap-5 w-full border-l border-slate-700 md:p-8 p-0 mt-5 md:mt-0 pl-8 pb-8" key={item?.degree}>
+                                <div className="md:w-1/4 w-full flex flex-row items-center md:items-start md:flex-col gap-3 md:gap-0">
                                     <p>{item?.duration}</p>
+                                    <p className="text-md text-[#5eead4]">{item?.percentage}</p>
                                 </div>
-                                <div className="md:w-3/4 w-full">
-                                    <div className="w-full">
-                                        <h1 className="text-[#fff] text-xl">{item?.institution}</h1>
-                                        <p className="mt-2 text-[#fdfeff]">{item?.degree}</p>
+                                <div className="flex flex-row justify-start items-center gap-5">
+                                    {item?.logo && <Image src={item?.logo} alt={item?.institution} className="h-12 w-12 object-cover rounded-full" />}
+                                    <div>
+                                        <Link href={item?.website} target="_blank">
+                                            <h1 className="text-[#fff] text-xl font-semibold hover:text-[#5eead4]">{item?.institution}</h1>
+                                        </Link>
+                                        <p className="text-md text-[#94a3b8]">{item?.degree}</p>
                                     </div>
                                 </div>
                             </div>
                         })
                     }
                 </section>
-
 
             </div>
         </div>
--- app/components/utils/common.tsx
+++ app/components/utils/common.tsx
@@ -1,8 +1,15 @@
+import Constient from '../../../public/constient_global_solutions_logo.jpg'
+import Axis from '../../../public/axis.png'
+import School from '../../../public/school.jpg'
+import College from '../../../public/kongu_engineering.jpg'
+
 export const ExperienceArray = [
     {
         company: 'Constient Global Solutions',
+        logo: Constient,
         position: 'Junior Software Engineer',
         duration: 'May 2022 - Present',
+        website:'https://www.constient.com/',
         description: 'Developed & optimized React.js applications, increasing user engagement by 15%. Led UI performance enhancements, reducing load time by 25% and improving accessibility. Troubleshot and resolved high-priority production issues, improving system efficiency by 30% under strict deadlines, Designed & implemented Redux state management, ensuring scalable & responsive applications. Integrated & secured APIs, working with backend teams for seamless data flow. Reviewed code & mentored junior developers, ensuring best coding practices and clean architecture.',
         skills: ['React.js', 'JavaScript (ES6+)', 'Next.js', 'TypeScript', 'HTML', 'CSS', 'Tailwind CSS', 'Material-UI', 'Bootstrap', 'Reactstrap', 'Redux', 'Git']
     }
@@ -12,13 +19,33 @@ export const EducationArray = [
     {
         institution: 'Kongu Engineering College',
         degree: 'Bachelor of Engineering in Mechatronics',
-        duration: '2017 - 2021'
+        duration: '2017 - 2021',
+        website:'https://kongu.ac.in/',
+        logo: College,
+        percentage:'69%'
+    },
+    {
+        institution: 'Shri Mahaa Higher Secondary School',
+        degree: 'HSC',
+        duration: '2017',
+        website:'https://schools.org.in/erode/33100501210/sri-mahaa-higher-secondary-school-ingur.html',
+        logo: School,
+        percentage:'81%'
+    },
+    {
+        institution: 'Shri Mahaa Higher Secondary School',
+        degree: 'SSLC',
+        duration: '2015',
+        website:'https://schools.org.in/erode/33100501210/sri-mahaa-higher-secondary-school-ingur.html',
+        logo: School,
+        percentage:'92%'
     }
 ]
 
 export const PrjectsArray = [
     {
         company: 'Constient Global Solutions',
+        logo: Axis,
         project_title: 'Axis CS',
         projects: [
             {
